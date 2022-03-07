bea9479f3c652a6dd17788cdc64757ffe916b45f
song.cao <song.cao@zmdesk.com>
2022-03-07 11:16:17 +0800

    快速排序

---
+++ src/main/java/algorithm/sort/QuickSort.java
@@ -0,0 +1,38 @@
+package algorithm.sort;
+
+public class QuickSort {
+
+
+    public static void main(String[] args) {
+        int[] array = new int[]{1, 8, 7, 6, 3, 2, 10};
+        int[] result = quickSort(array, 0, array.length - 1);
+    }
+
+    public static int[] quickSort(int[] array, int left, int right) {
+        if (left < right) {
+            int partitionIndex = partition(array, left, right);
+            quickSort(array, left, partitionIndex - 1);
+            quickSort(array, partitionIndex + 1, right);
+        }
+        return array;
+    }
+
+    public static int partition(int[] array, int left, int right) {
+        int pivot = left;
+        int index = pivot + 1;
+        for (int i = index; i <= right; i++) {
+            if (array[i] < array[pivot]) {
+                swap(array, i, index);
+                index++;
+            }
+        }
+        swap(array, pivot, index - 1);
+        return index - 1;
+    }
+
+    public static void swap(int[] array, int i, int j) {
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+}
